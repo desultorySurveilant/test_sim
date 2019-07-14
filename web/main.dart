355cb4862a2dac9339af09e60d71a48680426906
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-07-14 15:28:59 -0500

    better name randomizing

--- web/main.dart
+++ web/main.dart
@@ -8,11 +8,6 @@ void main() {
   querySelector('#output').text = 'Your name is $name';
 }
 String randName(){
-  switch (rand.nextInt(1)) {
-    case 0:
-      return 'Alice';
-      break;
-    default:
-      return 'UNSPEAKABLE';
-  }
+  List nameList = ['Alice', 'Bob', 'Claire'];
+  return nameList[rand.nextInt(nameList.length)];
 } 
