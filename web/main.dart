41845da7ef8966d06cb894e52e49c89aedccd974
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-07-14 14:51:57 -0500

    Randomly generates a name

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,18 @@
 import 'dart:html';
+import 'dart:math';
+
+Random rand = Random();
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  var name = randName();
+  querySelector('#output').text = 'Your name is $name';
 }
+String randName(){
+  switch (rand.nextInt(1)) {
+    case 0:
+      return 'Alice';
+      break;
+    default:
+      return 'UNSPEAKABLE';
+  }
+} 
