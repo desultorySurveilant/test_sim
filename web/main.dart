9e59bf0118f8f74d2bfd3b55ae5d1db0e9d5a6e2
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-07-17 16:28:57 -0500

    Removed magic numbers, added better party members

--- web/main.dart
+++ web/main.dart
@@ -7,17 +7,25 @@ void main() {
   List party = [randName()];
   var badName = randBadName();
   bool bossFight = false;
+  int maxParty = 6;
+  double bossFightOdds = 0.2;
+  double oddsIncrementPerRecruit = 0.1;
+
   Element e = querySelector('#output')
-    ..text = "Your name is ${party[0]}\n"
+    ..text = "You are a young ${party[0]}\n"
     ..appendText('You are preparing to go on an adventure\n')
     ..appendText('You must defeat the vile $badName who killed your parents\n')
     ..appendText('But first you must gather allies to aid you in this quest\n');
-  while(!bossFight && party.length <5){
+  while(!bossFight && party.length < maxParty){
     recruitMembers(party, e);
-    if(rand.nextInt(3) == 0)bossFight = true;
+    if(rand.nextDouble() < bossFightOdds){
+      bossFight = true;
+    }else{
+      bossFightOdds += oddsIncrementPerRecruit;
+    }
   }
   e.appendText('The party -- ${partyToString(party)} -- prepares to face the $badName\n');
-  if(party.length >= rand.nextInt(5)){
+  if(party.length >= rand.nextInt(maxParty)){
     e.appendText('The valiant ${party[0]} and their fellow companions have vanquished the $badName\n');
     e.appendText('They live out the rest of their lives happily and peacefully\n');
   }else {
@@ -26,7 +34,7 @@ void main() {
   }
 }
 String randName(){
-  List nameList = ['Leader1', 'Leader2', 'Leader3', 'Leader4'];
+  List nameList = ['Paladin', 'Wizard', 'Ranger', 'Cleric'];
   return nameList[rand.nextInt(nameList.length)];
 }
 String randBadName(){
@@ -34,8 +42,10 @@ String randBadName(){
   return nameList[rand.nextInt(nameList.length)];
 }
 void recruitMembers(List party, Element output){
-  party.add('dummy');
-  output.appendText('recruiting\n');
+  List followList = ['Warrior', 'Scholar', 'Hunter', 'Acolyte'];
+  String recruitee = followList[rand.nextInt(followList.length)];
+  party.add(recruitee);
+  output.appendText('You have recruited a new ${recruitee}!\n');
 }
 String partyToString(List l){
   if(l.isEmpty)return 'No members in the party';
